674d66fdb27c5515f318f3110baecc295fb28d2a
jagdish <jaagddissh@gmail.com>
2025-08-31 00:23:36 +0530

    WIP: API Gateway dev Dockerfile and YAML

---
+++ backend/api-gateway/src/main/java/com/cloud_ide/api_gateway/config/GatewayRoutesConfig.java
@@ -0,0 +1,4 @@
+package com.cloud_ide.api_gateway.config;
+
+public class GatewayRoutesConfig {
+}
---
+++ backend/api-gateway/src/test/java/com/cloud_ide/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_ide.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
