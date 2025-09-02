ae2878e5e9be813762b90040786c18584d074f76
jagdish <jaagddissh@gmail.com>
2025-09-02 18:54:40 +0530

    feat(infra): add Eureka service and Docker setup for auth-service

---
+++ backend/eureka-service/src/main/java/com/cloud_ide/eureka_service/EurekaServiceApplication.java
@@ -0,0 +1,16 @@
+package com.cloud_ide.eureka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
---
+++ backend/eureka-service/src/test/java/com/cloud_ide/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_ide.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
