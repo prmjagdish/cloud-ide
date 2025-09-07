59f7d70b23ea8a55bf41e7916bf87ec3696a95d0
jagdish <jaagddissh@gmail.com>
2025-09-07 19:40:29 +0530

    fix(folder structure): cloud-ide-frontend to frontend

--- frontend/src/components/layoutcom/TopNavbar.jsx
+++ frontend/src/components/layoutcom/TopNavbar.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import LogoSection from "../topnavbar/logosection";
 import RunControls from "../topnavbar/RunControls";
 import DateTimeDisplay from "../topnavbar/DateTimeDisplay";
+import ProjectDropdown from "../topnavbar/ProjectDropdown";
 
 const TopNavbar = () => {
   return (
     <div className="w-full flex h-10 items-center justify-between bg-[#252526] text-white px-4 py-2 shadow-md">
-     <LogoSection />
-     <RunControls />
-     <DateTimeDisplay />
+      <div className="flex items-center gap-11">
+        <span className="font-bold text-lg">☁️</span>
+        <ProjectDropdown />
+      </div>
+
+      <RunControls />
+
+      <DateTimeDisplay />
     </div>
   );
 };
